539a006fa042db163efb3fe684bbac225fbf6238
leonardo <leotrujillo@vera.com.uy>
2016-12-15 00:42:14 -0300

    maquetando el login, primera parte

--- views/auth/multiple-local.php
+++ views/auth/multiple-local.php
@@ -7,8 +7,8 @@
     </div>
 
 <?php endif; ?>
-
-<h2><?php \MapasCulturais\i::_e('Entrar', 'multipleLocal'); ?></h2>
+<div class="box-registro col-5">
+    <h5 class="textcenter"><?php \MapasCulturais\i::_e('Entrar', 'multipleLocal'); ?></h5>
 
 <form action="<?php echo $login_form_action; ?>" method="POST">
 
@@ -22,11 +22,26 @@
     <input type="submit" value="<?php \MapasCulturais\i::esc_attr_e('Entrar', 'multipleLocal'); ?>" />
 
 </form>
+    <h5 class="textcenter"><?php \MapasCulturais\i::_e('Esqueci minha senha', 'multipleLocal'); ?></h5>
 
-<h2><?php \MapasCulturais\i::_e('Registrar-se', 'multipleLocal'); ?></h2>
+<form action="<?php echo $recover_form_action; ?>" method="POST">
+
+    <p><?php \MapasCulturais\i::_e('Para recuperar sua senha, informe o e-mail utilizado no cadastro.', 'multipleLocal'); ?></p>
+    <?php \MapasCulturais\i::_e('E-mail', 'multipleLocal'); ?>
+    <input type="text" name="email" value="" />
+    <br/><br/>
+
+    <input type="submit" value="<?php \MapasCulturais\i::esc_attr_e('Recuperar senha', 'multipleLocal'); ?>" />
+
+</form>
+</div>
+<div class="box-registro col-5">
+<h5 class="textcenter"><?php \MapasCulturais\i::_e('Registrar-se', 'multipleLocal'); ?></h5>
 
 <form action="<?php echo $register_form_action; ?>" method="POST">
 
+    
+        
     <?php \MapasCulturais\i::_e('Nome', 'multipleLocal'); ?>
     <input type="text" name="name" value="<?php echo $triedName; ?>" />
     <br/><br/>
@@ -40,24 +55,11 @@
     <input type="password" name="confirm_password" value="" />
 
     <input type="submit" value="<?php \MapasCulturais\i::esc_attr_e('Registrar-se', 'multipleLocal'); ?>" />
-
+    </div>
 </form>
 
-<h2><?php \MapasCulturais\i::_e('Esqueci minha senha', 'multipleLocal'); ?></h2>
-
-<form action="<?php echo $recover_form_action; ?>" method="POST">
-
-    <p><?php \MapasCulturais\i::_e('Para recuperar sua senha, informe o e-mail utilizado no cadastro.', 'multipleLocal'); ?></p>
-    <?php \MapasCulturais\i::_e('E-mail', 'multipleLocal'); ?>
-    <input type="text" name="email" value="" />
-    <br/><br/>
-
-    <input type="submit" value="<?php \MapasCulturais\i::esc_attr_e('Recuperar senha', 'multipleLocal'); ?>" />
-
-</form>
-
-
-<h2><?php \MapasCulturais\i::_e('Redes Sociais', 'multipleLocal'); ?></h2>
+<div class="box-registro col-5">
+<h5 class="textcenter"><?php \MapasCulturais\i::_e('Redes Sociais', 'multipleLocal'); ?></h5>
 
 
 <p><?php \MapasCulturais\i::_e('Utilize sua conta em outros serviços para autenticar-se', 'multipleLocal'); ?>:</p>
@@ -68,5 +70,4 @@
 
 <?php $app->applyHook('multipleLocalAuth.loginPage:end'); ?>
 
-
-<br/>
+</div>
