8a28047d3e57942106a557cb36b385d2a55c6732
Leo Germani <leogermani@hacklab.com.br>
2016-12-17 13:29:40 -0200

    esqueci minha senha oculto, ativado por javascript

--- assets/js/multipleLocal.js
+++ assets/js/multipleLocal.js
@@ -1,5 +1,13 @@
-jQuery(document).ready(function() {
+$(function() {
     
+    $('#multiple-login-recover').click(function() {
+        $('#multiple-login').hide();
+        $('#multiple-recover').show();
+    });
     
+    $('#multiple-login-recover-cancel').click(function() {
+        $('#multiple-login').show();
+        $('#multiple-recover').hide();
+    });
 
 });
--- views/auth/multiple-local.php
+++ views/auth/multiple-local.php
@@ -8,66 +8,84 @@
 
 <?php endif; ?>
 <div class="box-registro col-5">
-    <h5 class="textcenter"><?php \MapasCulturais\i::_e('Entrar', 'multipleLocal'); ?></h5>
-
-<form action="<?php echo $login_form_action; ?>" method="POST">
-
     
-    <?php \MapasCulturais\i::_e('E-mail', 'multipleLocal'); ?>
-    <input type="text" name="email" value="<?php echo $triedEmail; ?>" />
-    <br/><br/>
-    <?php \MapasCulturais\i::_e('Senha', 'multipleLocal'); ?>
-    <input type="password" name="password" value="" />
-
-    <input type="submit" value="<?php \MapasCulturais\i::esc_attr_e('Entrar', 'multipleLocal'); ?>" />
-
-</form>
-    <h5 class="textcenter"><?php \MapasCulturais\i::_e('Esqueci minha senha', 'multipleLocal'); ?></h5>
-
-<form action="<?php echo $recover_form_action; ?>" method="POST">
-
-    <p><?php \MapasCulturais\i::_e('Para recuperar sua senha, informe o e-mail utilizado no cadastro.', 'multipleLocal'); ?></p>
-    <?php \MapasCulturais\i::_e('E-mail', 'multipleLocal'); ?>
-    <input type="text" name="email" value="" />
-    <br/><br/>
-
-    <input type="submit" value="<?php \MapasCulturais\i::esc_attr_e('Recuperar senha', 'multipleLocal'); ?>" />
-
-</form>
-</div>
-<div class="box-registro col-5">
-<h5 class="textcenter"><?php \MapasCulturais\i::_e('Registrar-se', 'multipleLocal'); ?></h5>
-
-<form action="<?php echo $register_form_action; ?>" method="POST">
-
+    <div id="multiple-login">
     
-        
-    <?php \MapasCulturais\i::_e('Nome', 'multipleLocal'); ?>
-    <input type="text" name="name" value="<?php echo $triedName; ?>" />
-    <br/><br/>
-    <?php \MapasCulturais\i::_e('E-mail', 'multipleLocal'); ?>
-    <input type="text" name="email" value="<?php echo $triedEmail; ?>" />
-    <br/><br/>
-    <?php \MapasCulturais\i::_e('Senha', 'multipleLocal'); ?>
-    <input type="password" name="password" value="" />
-    <br/><br/>
-    <?php \MapasCulturais\i::_e('Confirmar senha', 'multipleLocal'); ?>
-    <input type="password" name="confirm_password" value="" />
+        <h5 class="textcenter"><?php \MapasCulturais\i::_e('Entrar', 'multipleLocal'); ?></h5>
 
-    <input type="submit" value="<?php \MapasCulturais\i::esc_attr_e('Registrar-se', 'multipleLocal'); ?>" />
+            <form action="<?php echo $login_form_action; ?>" method="POST">
+
+                
+                <?php \MapasCulturais\i::_e('E-mail', 'multipleLocal'); ?>
+                <input type="text" name="email" value="<?php echo $triedEmail; ?>" />
+                <br/><br/>
+                <?php \MapasCulturais\i::_e('Senha', 'multipleLocal'); ?>
+                <input type="password" name="password" value="" />
+
+                <input type="submit" value="<?php \MapasCulturais\i::esc_attr_e('Entrar', 'multipleLocal'); ?>" />
+                
+                <a id="multiple-login-recover"><?php \MapasCulturais\i::_e('Esqueci minha senha', 'multipleLocal'); ?></a>
+                
+            </form>
+            
     </div>
-</form>
+    
+    <div id="multiple-recover">
+    
+        <h5 class="textcenter"><?php \MapasCulturais\i::_e('Esqueci minha senha', 'multipleLocal'); ?></h5>
+
+        <form action="<?php echo $recover_form_action; ?>" method="POST">
+
+            <p><?php \MapasCulturais\i::_e('Para recuperar sua senha, informe o e-mail utilizado no cadastro.', 'multipleLocal'); ?></p>
+            <?php \MapasCulturais\i::_e('E-mail', 'multipleLocal'); ?>
+            <input type="text" name="email" value="" />
+            <br/><br/>
+
+            <input type="submit" value="<?php \MapasCulturais\i::esc_attr_e('Recuperar senha', 'multipleLocal'); ?>" />
+            
+            <a id="multiple-login-recover-cancel"><?php \MapasCulturais\i::_e('Cancelar', 'multipleLocal'); ?></a>
+
+        </form>
+        
+    </div>
+    
+</div>
 
 <div class="box-registro col-5">
-<h5 class="textcenter"><?php \MapasCulturais\i::_e('Redes Sociais', 'multipleLocal'); ?></h5>
+    <h5 class="textcenter"><?php \MapasCulturais\i::_e('Registrar-se', 'multipleLocal'); ?></h5>
+
+    <form action="<?php echo $register_form_action; ?>" method="POST">
+
+        
+            
+        <?php \MapasCulturais\i::_e('Nome', 'multipleLocal'); ?>
+        <input type="text" name="name" value="<?php echo $triedName; ?>" />
+        <br/><br/>
+        <?php \MapasCulturais\i::_e('E-mail', 'multipleLocal'); ?>
+        <input type="text" name="email" value="<?php echo $triedEmail; ?>" />
+        <br/><br/>
+        <?php \MapasCulturais\i::_e('Senha', 'multipleLocal'); ?>
+        <input type="password" name="password" value="" />
+        <br/><br/>
+        <?php \MapasCulturais\i::_e('Confirmar senha', 'multipleLocal'); ?>
+        <input type="password" name="confirm_password" value="" />
+
+        <input type="submit" value="<?php \MapasCulturais\i::esc_attr_e('Registrar-se', 'multipleLocal'); ?>" />
+        </div>
+    </form>
+
+</div>
+
+<div class="box-registro col-5">
+    <h5 class="textcenter"><?php \MapasCulturais\i::_e('Redes Sociais', 'multipleLocal'); ?></h5>
 
 
-<p><?php \MapasCulturais\i::_e('Utilize sua conta em outros serviços para autenticar-se', 'multipleLocal'); ?>:</p>
-<a href="<?php echo $app->createUrl('auth', 'facebook') ?>"><img src="<?php $this->asset('img/fb-login.png'); ?>" /></a>&nbsp;&nbsp;
-<a href="<?php echo $app->createUrl('auth', 'google') ?>"><img src="<?php $this->asset('img/go-login.png'); ?>" /></a>&nbsp;&nbsp;
-<a href="<?php echo $app->createUrl('auth', 'linkedin') ?>"><img src="<?php $this->asset('img/ln-login.png'); ?>" /></a>
-<!--<a href="<?php echo $app->createUrl('auth', 'twitter') ?>">Twitter</a> -->
+    <p><?php \MapasCulturais\i::_e('Utilize sua conta em outros serviços para autenticar-se', 'multipleLocal'); ?>:</p>
+    <a href="<?php echo $app->createUrl('auth', 'facebook') ?>"><img src="<?php $this->asset('img/fb-login.png'); ?>" /></a>&nbsp;&nbsp;
+    <a href="<?php echo $app->createUrl('auth', 'google') ?>"><img src="<?php $this->asset('img/go-login.png'); ?>" /></a>&nbsp;&nbsp;
+    <a href="<?php echo $app->createUrl('auth', 'linkedin') ?>"><img src="<?php $this->asset('img/ln-login.png'); ?>" /></a>
+    <!--<a href="<?php echo $app->createUrl('auth', 'twitter') ?>">Twitter</a> -->
 
-<?php $app->applyHook('multipleLocalAuth.loginPage:end'); ?>
+    <?php $app->applyHook('multipleLocalAuth.loginPage:end'); ?>
 
 </div>
