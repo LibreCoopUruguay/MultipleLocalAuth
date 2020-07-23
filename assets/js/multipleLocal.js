b706568e04770173bf81e9b06721fa17355d92a4
Aleff Bruno Rodrigues Silva <aleff.silva@secult.local>
2020-07-23 20:41:46 -0300

    adiciona validacao no js para saber se elementos existem antes de adicionar eventos nos mesmos

--- assets/js/multipleLocal.js
+++ assets/js/multipleLocal.js
@@ -37,75 +37,77 @@ $(function() {
     }
     
 
-    // verifica a força da senha
-    let passwordMustHaveCapitalLetters = /[A-Z]/;
-    let passwordMustHaveLowercaseLetters = /[a-z]/;
-    let passwordMustHaveSpecialCharacters = /[$@$!%*#?&]/;
-    let passwordMustHaveNumbers = /[0-9]/;
-    let minimumPasswordLength = 8;
-
-    let rules = [];
-
-    //faz uma requisição para pegar as configs de força de senha
-    $.get( `${MapasCulturais.baseURL}auth/passwordvalidationinfos`, function( data ) {
-
-        if(data.passwordRules.passwordMustHaveCapitalLetters) {
-            rules.push(passwordMustHaveCapitalLetters);
-            $("#passwordRulesUL").append("<li>A senha deve conter uma letra maiúsculas</li>");
-        }
-
-        if(data.passwordRules.passwordMustHaveLowercaseLetters) {
-            rules.push(passwordMustHaveLowercaseLetters);
-            $("#passwordRulesUL").append("<li>A senha deve conter uma letra minúsculas</li>");
-        }
-
-        if(data.passwordRules.passwordMustHaveSpecialCharacters) {
-            rules.push(passwordMustHaveSpecialCharacters);
-            $("#passwordRulesUL").append("<li>A senha deve conter um caractere especial</li>");
-        }
-
-        if(data.passwordRules.passwordMustHaveNumbers) {
-            rules.push(passwordMustHaveNumbers);
-            $("#passwordRulesUL").append("<li>A senha deve conter um numero</li>");
-        }
-
-        if(data.passwordRules.minimumPasswordLength) {
-            minimumPasswordLength = data.passwordRules.minimumPasswordLength
-        }
-        $("#passwordRulesUL").append(`<li>A senha deve conter no minimo ${minimumPasswordLength} digitos</li>`);
-
-        console.log("get passwordvalidationinfos OK");
-    });
-
     var password = document.getElementById("pwd-progress-bar-validation");
-    password.addEventListener('keyup', function() {
-        var pwd = password.value
-        
-        // Reset if password length is zero
-        if (pwd.length === 0) {
-            document.getElementById("progresslabel").innerHTML = "";
-            document.getElementById("progress").value = "0";
-            return;
-        }
+    if(password) {
+        // verifica a força da senha
+        let passwordMustHaveCapitalLetters = /[A-Z]/;
+        let passwordMustHaveLowercaseLetters = /[a-z]/;
+        let passwordMustHaveSpecialCharacters = /[$@$!%*#?&]/;
+        let passwordMustHaveNumbers = /[0-9]/;
+        let minimumPasswordLength = 8;
 
-        let rulesLength = rules.length;
+        let rules = [];
 
-        var prog = rules
-            .reduce((memo, test) => memo + test.test(pwd), 0);
+        //faz uma requisição para pegar as configs de força de senha
+        $.get( `${MapasCulturais.baseURL}auth/passwordvalidationinfos`, function( data ) {
+
+            if(data.passwordRules.passwordMustHaveCapitalLetters) {
+                rules.push(passwordMustHaveCapitalLetters);
+                $("#passwordRulesUL").append("<li>A senha deve conter uma letra maiúsculas</li>");
+            }
+
+            if(data.passwordRules.passwordMustHaveLowercaseLetters) {
+                rules.push(passwordMustHaveLowercaseLetters);
+                $("#passwordRulesUL").append("<li>A senha deve conter uma letra minúsculas</li>");
+            }
+
+            if(data.passwordRules.passwordMustHaveSpecialCharacters) {
+                rules.push(passwordMustHaveSpecialCharacters);
+                $("#passwordRulesUL").append("<li>A senha deve conter um caractere especial</li>");
+            }
+
+            if(data.passwordRules.passwordMustHaveNumbers) {
+                rules.push(passwordMustHaveNumbers);
+                $("#passwordRulesUL").append("<li>A senha deve conter um numero</li>");
+            }
+
+            if(data.passwordRules.minimumPasswordLength) {
+                minimumPasswordLength = data.passwordRules.minimumPasswordLength
+            }
+            $("#passwordRulesUL").append(`<li>A senha deve conter no minimo ${minimumPasswordLength} digitos</li>`);
+
+            console.log("get passwordvalidationinfos OK");
+        });
 
 
-        let percentToAdd = (100 / (rulesLength+1));
-        let currentPercentPasswordCorrect = ((prog*100) / (rulesLength+1));
-        if(pwd.length > minimumPasswordLength-1) {
-            currentPercentPasswordCorrect = currentPercentPasswordCorrect + percentToAdd
-        }
+        password.addEventListener('keyup', function() {
+            var pwd = password.value
+            
+            // Reset if password length is zero
+            if (pwd.length === 0) {
+                document.getElementById("progresslabel").innerHTML = "";
+                document.getElementById("progress").value = "0";
+                return;
+            }
 
-        document.getElementById("progresslabel").innerHTML = `${currentPercentPasswordCorrect.toFixed(2)}%`;
-        document.getElementById("progress").value = `${currentPercentPasswordCorrect.toFixed(2)}`;
+            let rulesLength = rules.length;
+
+            var prog = rules
+                .reduce((memo, test) => memo + test.test(pwd), 0);
 
 
+            let percentToAdd = (100 / (rulesLength+1));
+            let currentPercentPasswordCorrect = ((prog*100) / (rulesLength+1));
+            if(pwd.length > minimumPasswordLength-1) {
+                currentPercentPasswordCorrect = currentPercentPasswordCorrect + percentToAdd
+            }
+
+            document.getElementById("progresslabel").innerHTML = `${currentPercentPasswordCorrect.toFixed(2)}%`;
+            document.getElementById("progress").value = `${currentPercentPasswordCorrect.toFixed(2)}`;
+
+        });
+    }
     
-    });
 
     $('#multiple-login-recover').click(function() {
         $('#multiple-login').hide();
