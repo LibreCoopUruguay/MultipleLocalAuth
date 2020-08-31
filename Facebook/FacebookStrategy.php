c2155ad0b5ec943d46ac68da2ac59adfb7358786
Ben Rainir <benrainir@gmail.com>
2020-08-31 17:32:36 -0300

    corrigido parse do json para array do response com o dados do access token

--- Facebook/FacebookStrategy.php
+++ Facebook/FacebookStrategy.php
@@ -60,7 +60,8 @@ class FacebookStrategy extends OpauthStrategy{
 			);
 			$response = $this->serverGet($url, $params, null, $headers);
 
-			parse_str($response, $results);
+			//response json to array
+			$results = json_decode($response, true);
 
 			if (!empty($results) && !empty($results['access_token'])){
 				$me = $this->me($results['access_token']);
