03a2cc1b253cf236a056315bd52b6bb6e9ad60a0
Isaque Melo <isaquegbmelo2@gmail.com>
2020-09-01 18:12:19 -0300

    Remove link de suporte sem sentido

--- views/auth/multiple-local.php
+++ views/auth/multiple-local.php
@@ -248,8 +248,4 @@ function showStrategy($name, $config) {
         </div>
 
     </div>
-
-    <div class="help-section">
-        Precisa de ajuda? <a href="https://bit.ly/3hOQfBz" target="_blank"> Clique aqui </a> para falar com nossa equipe de suporte por chat. Ou envie um email para <a href="mailto:suportemapaculturalpa@gmail.com" target="_blank"> suportemapaculturalpa@gmail.com </a>
-    </div>
 </div>
