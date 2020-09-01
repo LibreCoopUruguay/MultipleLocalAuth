8c2bab88b5185791e22cefdec34a736d453c57bb
Isaque Melo <isaquegbmelo2@gmail.com>
2020-09-01 16:20:50 -0300

    Adiciona links suporte no login

--- views/auth/multiple-local.php
+++ views/auth/multiple-local.php
@@ -248,4 +248,8 @@ function showStrategy($name, $config) {
         </div>
 
     </div>
+
+    <div class="help-section">
+        Precisa de ajuda? <a href="https://bit.ly/3hOQfBz" target="_blank"> Clique aqui </a> para falar com nossa equipe de suporte por chat. Ou envie um email para <a href="mailto:suportemapaculturalpa@gmail.com" target="_blank"> suportemapaculturalpa@gmail.com </a>
+    </div>
 </div>
