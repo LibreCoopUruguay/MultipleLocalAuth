c215eaf67504818cb84b6bb9acc0d1e42e30617b
Raul Vitor Lucena Brito <raulvitorcc@gmail.com>
2023-02-02 17:02:17 -0300

    Remove método duplicado (Ref.: #51)

--- Provider.php
+++ Provider.php
@@ -940,35 +940,6 @@ class Provider extends \MapasCulturais\AuthProvider {
     
     }
 
-    function validaCPF($cpf) {
- 
-        // Extrai somente os números
-        $cpf = preg_replace( '/[^0-9]/is', '', $cpf );
-         
-        // Verifica se foi informado todos os digitos corretamente
-        if (strlen($cpf) != 11) {
-            return false;
-        }
-    
-        // Verifica se foi informada uma sequência de digitos repetidos. Ex: 111.111.111-11
-        if (preg_match('/(\d)\1{10}/', $cpf)) {
-            return false;
-        }
-    
-        // Faz o calculo para validar o CPF
-        for ($t = 9; $t < 11; $t++) {
-            for ($d = 0, $c = 0; $c < $t; $c++) {
-                $d += $cpf[$c] * (($t + 1) - $c);
-            }
-            $d = ((10 * $d) % 11) % 10;
-            if ($cpf[$c] != $d) {
-                return false;
-            }
-        }
-        return true;
-    
-    }
-
     function doLogin() {
         $app = App::i();
         $config = $this->_config;
@@ -1000,7 +971,7 @@ class Provider extends \MapasCulturais\AuthProvider {
         $pass = filter_var($app->request->post('password'), FILTER_SANITIZE_STRING);
 
         // verifica se esta habilitado 'enableLoginByCPF' em conf.php && esta tentando fazer login com CPF
-        if ($this->validaCPF($email) && $config['enableLoginByCPF']) {
+        if ($this->validateCPF($email) && $config['enableLoginByCPF']) {
 
             // LOGIN COM CPF
             $metadataFieldCpf = $this->getMetadataFieldCpfFromConfig(); 
