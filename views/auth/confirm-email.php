75fe1da196e2729d7480f5ee29c9e98776d441cb
Raul Vitor Lucena Brito <raulvitorcc@gmail.com>
2023-02-10 00:40:51 -0300

    Criação e estilização da tela de confirmação de email (Ref.: #59)

--- views/auth/confirm-email.php
+++ views/auth/confirm-email.php
@@ -1,15 +1,29 @@
-<div class="info-message">
-    <div class="status-card">
-        <div class="status-card--title">
-            <?php \MapasCulturais\i::_e('Informação', 'multipleLocal'); ?>
-        </div>
-        <div class="status-card--content">
-            <div class="content">
-                <?= $msg ?>
+<?php
+/**
+ * @var \MapasCulturais\Themes\BaseV2\Theme $this
+ * @var \MapasCulturais\App $app
+ * 
+ */
+
+use MapasCulturais\i;
+
+$this->import('
+    mapas-card
+');
+?>
+
+<div class="confirm-email">
+    <mapas-card class="no-title">
+        <template #content>
+            <div class="grid-12">
+                <div class="col-12 header">
+                    <label class="header__title"> <?= i::__('Alteração de senha') ?> </label>
+                    <mc-icon name="circle-checked" class="header__icon"></mc-icon>
+                    <label class="header__label"> <?= i::__('Enviamos as instruções de alteração de senha para seu e-mail.') ?> </label>
+                </div>
+
+                <a class="col-12 button button--primary button--large button--md" href="<?= $app->createUrl('auth') ?>" type="submit"> <?= i::__('Entrar na minha conta') ?> </a>
             </div>
-            <a href="/" class="btn">
-                <?php \MapasCulturais\i::_e('Clique aqui para entrar no sistema', 'multipleLocal'); ?>
-            </a>
-        </div>
-    </div>
+        </template>
+    </mapas-card>
 </div>
