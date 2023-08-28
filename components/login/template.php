14b0ac80c389d492871089b2590d8e99e7068c21
Raul Vitor Lucena Brito <raulvitorcc@gmail.com>
2023-08-28 10:46:00 -0300

    Ajusta tela de login de acordo com modificações do figma (Ref.: #2964)

--- components/login/template.php
+++ components/login/template.php
@@ -17,59 +17,81 @@ $this->import('
     <!-- Login action -->
 
     <div v-if="!recoveryRequest && !recoveryMode" class="login__action">
-        <mc-card>
-            <template #title>
-                <label> <?= $this->text('welcome', i::__('Boas vindas!')) ?> </label>
-                <p> <?= $this->text('greeting', sprintf(i::__('Entre na sua conta do %s'), $app->siteName)) ?> </p>
-            </template>
-            <template #content>
-                <form class="grid-12" @submit.prevent="doLogin();">
-                    <div class="field col-12">
-                        <label for="email"> <?= i::__('E-mail ou CPF') ?> </label>
-                        <input type="text" name="email" id="email" v-model="email" autocomplete="off" />
-                    </div>
-                    <div class="field col-12 password">
-                        <label for="password"> <?= i::__('Senha') ?> </label>
-                        <input type="password" name="password" id="password" v-model="password" autocomplete="off" />
-                        <a id="multiple-login-recover" class="recover" @click="recoveryRequest = true"> <?= i::__('Esqueci minha senha') ?> </a>
-                        <div class="seePassword" @click="togglePassword('password', $event)"></div>
-                    </div>                    
-                    <VueRecaptcha v-if="configs['google-recaptcha-sitekey']" :sitekey="configs['google-recaptcha-sitekey']" @verify="verifyCaptcha" @expired="expiredCaptcha" @render="expiredCaptcha" class="g-recaptcha col-12"></VueRecaptcha>
-                    <button class="col-12 button button--primary button--large button--md" type="submit"> <?= i::__('Entrar') ?> </button>
-                    
-                    <div class="divider col-12"></div>
 
-                    <div class="social-login col-12">
-                        <a v-if="configs.strategies.govbr?.visible" class="social-login--button button button--icon button--large button--md govbr" href="<?php echo $app->createUrl('auth', 'govbr') ?>">                                
-                            <div class="img"> <img height="16" class="br-sign-in-img" src="<?php $this->asset('img/govbr-white.png'); ?>" /> </div>                                
-                            <?= i::__('Entrar com Gov.br') ?>                            
-                        </a>
-                        <a v-if="configs.strategies.Google?.visible" class="social-login--button button button--icon button--large button--md google" href="<?php echo $app->createUrl('auth', 'google') ?>">                                
-                            <div class="img"> <img height="16" src="<?php $this->asset('img/g.png'); ?>" /> </div>                                
-                            <?= i::__('Entrar com Google') ?>
-                        </a>
-                        <h1 v-if="configs.strategies.Google?.visible || configs.strategies.govbr?.visible" class="col-12">OU</h1>
-                        <div class="create col-12">
-                            <small> <?= i::__('Crie sua conta para começar a usar o Mapas agora mesmo.') ?> </small>    
-                            <a class="col-12 button button--primary button--large button--md" href="<?php echo $app->createUrl('auth', 'register') ?>"> 
-                                <?= i::__('Criar conta') ?>
+        <div class="login__card">
+            <div class="login__card__header">
+                <h3> <?= $this->text('welcome', i::__('Boas vindas!')) ?> </h3>
+                <h6> <?= $this->text('greeting', sprintf(i::__('Entre na sua conta do %s'), $app->siteName)) ?> </h6>
+            </div>
+
+            <div class="login__card__content">
+                <form class="login__form" @submit.prevent="doLogin();">
+                    <div class="login__fields">
+                        <div class="field">
+                            <label for="email"> <?= i::__('E-mail ou CPF') ?> </label>
+                            <input type="text" name="email" id="email" v-model="email" autocomplete="off" />
+                        </div>
+
+                        <div class="field password">
+                            <label for="password"> <?= i::__('Senha') ?> </label>
+                            <input type="password" name="password" id="password" v-model="password" autocomplete="off" />
+                            <a id="multiple-login-recover" class="login__recover-link" @click="recoveryRequest = true"> <?= i::__('Esqueci minha senha') ?> </a>
+                            <div class="seePassword" @click="togglePassword('password', $event)"></div>
+                        </div> 
+                    </div>                     
+
+                    <VueRecaptcha v-if="configs['google-recaptcha-sitekey']" :sitekey="configs['google-recaptcha-sitekey']" @verify="verifyCaptcha" @expired="expiredCaptcha" @render="expiredCaptcha" class="g-recaptcha"></VueRecaptcha>
+                    
+                    <div class="login__buttons">
+                        <button class=" button button--primary button--large button--md" type="submit"> <?= i::__('Entrar') ?> </button>
+
+                        <div v-if="configs.strategies.Google?.visible || configs.strategies.govbr?.visible" class="divider"> 
+                            <span class="divider__text"> <?= i::__('Ou entre com') ?> </span>
+                        </div>
+
+                        <div class="login__social-buttons">
+                            <a v-if="configs.strategies.govbr?.visible" class="social-login--button button button--icon button--large button--md govbr" href="<?php echo $app->createUrl('auth', 'govbr') ?>">                                
+                                <div class="img"> <img height="16" class="br-sign-in-img" src="<?php $this->asset('img/govbr-white.png'); ?>" /> </div>                                
+                                <?= i::__('Entrar com Gov.br') ?>                            
+                            </a>
+
+                            <a v-if="configs.strategies.Google?.visible" class="social-login--button button button--icon button--large button--md google" href="<?php echo $app->createUrl('auth', 'google') ?>">                                
+                                <div class="img"> <img height="16" src="<?php $this->asset('img/g.png'); ?>" /> </div>                                
+                                <?= i::__('Entrar com Google') ?>
+                            </a>
+
+                            <a v-if="configs.strategies.govbr?.visible" class="social-login--button button button--md button--large govbr" href="<?php echo $app->createUrl('auth', 'govbr') ?>">                                
+                                <div class="img"> <img height="16" class="br-sign-in-img" src="<?php $this->asset('img/govbr-white.png'); ?>" /> </div>                        
+                            </a>
+
+                            <a v-if="configs.strategies.Google?.visible" class="social-login--button button button--icon button--md button--large google" href="<?php echo $app->createUrl('auth', 'google') ?>">                                
+                                <div class="img"> <img height="16" src="<?php $this->asset('img/g.png'); ?>" /> </div>                                
+                                <?= i::__('Google') ?>
                             </a>
                         </div>
                     </div>
+
+                    <div class="create ">
+                        <h5 class="bold"> <?= $this->text('register', sprintf(i::__('Ainda não tem cadastro no %s? Realize seu cadastro agora!'), $app->siteName)) ?> </h5>
+
+                        <a class=" button button--primary button--large button--md" href="<?php echo $app->createUrl('auth', 'register') ?>"> 
+                            <?= i::__('Criar conta') ?>
+                        </a>
+                    </div>
                 </form>
-            </template>
-        </mc-card>
+            </div>
+        </div>
     </div>
 
     <!-- Recovery request -->
-
     <div v-if="recoveryRequest" class="login__recovery--request">
-        <mc-card v-if="!recoveryEmailSent">
-            <template #title>
-                <label> <?= i::__('Alteração de senha') ?> </label>
-                <p> <?= i::__('Se você esqueceu a senha, não se preocupe, todo mundo passa por isso.') ?> <br> <?= i::__('Digite seu e-mail para criar uma nova.') ?> </p>
-            </template>
-            <template #content>
+        <div class="login__card" v-if="!recoveryEmailSent">
+            <div class="login__card__header">
+                <h3> <?= i::__('Alteração de senha') ?> </h3>
+                <h6> <?= i::__('Se você esqueceu a senha, não se preocupe, todo mundo passa por isso.') ?> <br> <?= i::__('Digite seu e-mail para criar uma nova.') ?> </p>
+            </div>
+
+            <div class="login__card__content">
                 <form class="grid-12" @submit.prevent="requestRecover();">
                     <div class="field col-12">
                         <label for="email"> <?= i::__('E-mail') ?> </label>
@@ -79,11 +101,11 @@ $this->import('
                     <button class="col-12 button button--primary button--large button--md" type="submit"> <?= i::__('Alterar senha') ?> </button>
                     <a @click="recoveryRequest = false" class="col-12 button button--secondarylight button--large button--md"> <?= i::__('Voltar') ?> </a>
                 </form>
-            </template>
-        </mc-card>
+            </div>
+        </div>
 
-        <mc-card class="no-title" v-if="recoveryEmailSent">
-            <template #content>
+        <div class="login__card" v-if="recoveryEmailSent">
+            <div class="login__card__content">
                 <div class="grid-12">
                     <div class="col-12 header">
                         <label class="header__title"> <?= i::__('Alteração de senha') ?> </label>
@@ -94,20 +116,19 @@ $this->import('
                     <button class="col-12 button button--primary button--large button--md" type="submit"> <?= i::__('Não recebi o e-mail') ?> </button>
                     <a @click="recoveryEmailSent = false" class="col-12 button button--secondarylight button--large button--md"> <?= i::__('Voltar') ?> </a>
                 </div>
-            </template>
-        </mc-card>
+            </div>
+        </div>
     </div>
 
     <!-- Recovery action -->
-
     <div v-if="recoveryMode" class="login__recovery--action">
-        <mc-card>
-            <template #title>
-                <label> <?= i::__('Redefinir senha de acesso') ?> </label>
-            </template>
-            <template #content>
-                <form class="grid-12" @submit.prevent="doRecover();">
+        <div class="login__card" v-if="recoveryEmailSent">
+            <div class="login__card__header">
+                <h3> <?= i::__('Redefinir senha de acesso') ?> </h3>
+            </div>
 
+            <div class="login__card__content">
+                <form class="grid-12" @submit.prevent="doRecover();">
                     <div class="field col-12 password">
                         <label for="pwd"> <?= i::__('Senha'); ?> </label>
                         <input autocomplete="off" id="pwd" type="password" name="password" v-model="password" />
@@ -124,8 +145,7 @@ $this->import('
 
                     <button class="col-12 button button--primary button--large button--md" type="submit"> <?= i::__('Redefinir senha') ?> </button>
                 </form>
-            </template>
-        </mc-card>
+            </div>
+        </div>
     </div>
-
 </div>
